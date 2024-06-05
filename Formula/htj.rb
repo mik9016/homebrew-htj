2d88cadadcce63e4e653f9333c3fd78a08506966
Mikolaj Gruszecki <mikolaj.gruszecki@sdui.de>
2024-06-05 12:36:02 +0200

    tag change

--- Formula/htj.rb
+++ Formula/htj.rb
@@ -1,7 +1,7 @@
 class Htj < Formula
   desc "htj - HEIC to JPG Converter"
   homepage "https://github.com/mik9016/homebrew-htj"
-  url "https://github.com/mik9016/homebrew-htj/archive/refs/tags/htj_1.0.tar.gz"
+  url "https://github.com/mik9016/homebrew-htj/archive/refs/tags/htj_1.1.tar.gz"
   sha256 "a1f3fc10469b1617262a13d738f06aecd0b2d1ec08e194070d68c66daff1a61f"
   license "MIT"
 
